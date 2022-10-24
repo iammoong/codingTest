440e12fc4701f74900febd79c1806973d279f332
iammoong <ansrlwkd225@naver.com>
2022-10-24 10:25:19 +0900

    test

--- coding/src/BackJoon/PoketmonMaster.java
+++ coding/src/BackJoon/PoketmonMaster.java
@@ -3,18 +3,36 @@ package BackJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class PoketmonMaster {
 
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken()); // 입력할 포켓몬 갯수
+		int M = Integer.parseInt(st.nextToken()); // 내가 찾을 포켓몬
+
+		Map<String, String> map = new HashMap<>();
+
+		for (int i = 0; i < N; i++) {
+			String poketmon = br.readLine(); // 포켓몬 이름 입력
+			String number = Integer.toString(i + 1); // 포켓몬 번호입력
+			map.put(poketmon, number); // Hashmap에 넣기
+			map.put(number, poketmon);
+		} // 포켓몬 이름, 번호 입력
+
+		StringBuilder sb = new StringBuilder(M);
+
+		for (int i = 0; i < M; i++) {
+			sb.append(map.get(br.readLine()) + "\n");
+		} // 내가 맞춰야할 포켓몬 번호, 이름 입력
+
+		System.out.println(sb);
 
 	}
 
