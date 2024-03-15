af1e41e24d8e7f6f0fc01e2b6c9b66f7b06bbe01
iammoong <ansrlwkd225@naver.com>
2024-03-15 21:59:03 +0900

    백준A-B

---
+++ CodingProject/src/Day_1/AMinusB.java
@@ -0,0 +1,18 @@
+package Day_1;
+
+import java.util.Scanner;
+
+public class AMinusB {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		System.out.println(A - B);
+		
+		scanner.close();
+
+	}
+
+}
